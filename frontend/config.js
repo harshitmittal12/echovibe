879b371eb3d77accc436a55af4f290a365e5ecff
Raghav707 <gupta456raghav@gmail.com>
2026-05-03 22:21:09 +0530

    updated api

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,7 @@
  * Change API_BASE_URL when deploying to production
  */
 const ECHOVIBE_CONFIG = {
-  API_BASE_URL: "http://localhost:5000",
+  API_BASE_URL: "", // Leave empty for production (relative to current domain)
   JIOSAAVN_API: "https://saavn.sumit.co/api",
   ITUNES_API: "https://itunes.apple.com",
 };
